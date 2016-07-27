d1859f7bb1b606f9d696221f083f5957e0452c72
Michael Jackson <mjijackson@gmail.com>
2016-07-26 22:07:16 -0700

    Clean up Karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
 
+const projectName = 'react-style'
+
 module.exports = (config) => {
   const customLaunchers = {
     BS_Chrome: {
@@ -84,31 +86,22 @@ module.exports = (config) => {
   if (process.env.USE_CLOUD) {
     config.browsers = Object.keys(customLaunchers)
     config.reporters = [ 'dots' ]
+    config.concurrency = 2
+
     config.browserDisconnectTimeout = 10000
     config.browserDisconnectTolerance = 3
-    config.browserNoActivityTimeout = 30000
-    config.captureTimeout = 120000
 
     if (process.env.TRAVIS) {
-      const buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')'
-
       config.browserStack = {
-        username: process.env.BROWSER_STACK_USERNAME,
-        accessKey: process.env.BROWSER_STACK_ACCESS_KEY,
-        pollingTimeout: 10000,
-        startTunnel: true,
-        project: 'history',
-        build: buildLabel,
+        project: projectName,
+        build: process.env.TRAVIS_BUILD_NUMBER,
         name: process.env.TRAVIS_JOB_NUMBER
       }
 
       config.singleRun = true
     } else {
       config.browserStack = {
-        username: process.env.BROWSER_STACK_USERNAME,
-        accessKey: process.env.BROWSER_STACK_ACCESS_KEY,
-        pollingTimeout: 10000,
-        startTunnel: true
+        project: projectName
       }
     }
   }
